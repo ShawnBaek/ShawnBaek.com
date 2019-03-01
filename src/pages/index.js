525e3ff144818036fa9eb61ec1b706fd7f3b66d2
ShawnBaek <shawn@shawnbaek.com>
2019-03-01 23:46:20 +0900

    Update Index.js Successfully render the WordPress contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,24 @@ class IndexPage extends Component {
   render() {
     const data = this.props.data.wordpressPage
     return (
+      <Layout>
+      <Headline title={"I'm Shawn Baek"} subTitle={"iOS Developer"}/>
+      <div
+         style={{
+           margin: '0 auto',
+           maxWidth: 800,
+           padding: '0px 1.0875rem 1.45rem',
+           paddingTop: '1.45rem',
+         }}
+      >
+      <h3>Hellow!!</h3>
       <div>
-        {data.title}
-
-        {data.content}
-
-        <h1>H....</h1>
+          <h1>{data.title}</h1>
+          <h3 style={{color:'rgb(76, 76, 76)'}}>{data.title}</h3>
+          <div style={{color:'rgb(76, 76, 76)'}} dangerouslySetInnerHTML={{ __html: data.content }}></div>
       </div>
+      </div>
+    </Layout>
     )
   }
 }
